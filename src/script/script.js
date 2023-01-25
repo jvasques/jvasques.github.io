25e856046edcdc8e550dced0fd87a3a3e478bcc8
VasquesJP <vaquesjp@hotmail.com>
2023-01-25 17:25:19 -0300

    add new project

--- src/script/script.js
+++ src/script/script.js
@@ -83,3 +83,16 @@ function autoThemeMode() {
         sun.style.display = "none";
     }
 }
+$(window).on("load scroll", function(){
+    $(".shows").each(function(){
+        var el = $(this);
+        var eleHeight = el.outerHeight(); // altura da div
+        var eleTopo = el.offset().top; // distancia da div ao topo do documento
+        var scrlTopo = $(window).scrollTop(); // quanto foi rolada a janela
+        var distance = eleTopo-scrlTopo; // distancia da div ao topo da janela
+        var altJanela = window.innerHeight; // altura da janela
+        if(distance <= altJanela-(eleHeight/1.5)) {
+            el.animate({ 'opacity': 1 }, 400);
+        }
+    });
+});
